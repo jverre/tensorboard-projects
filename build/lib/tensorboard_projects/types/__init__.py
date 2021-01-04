9ef0c294ca56d13c14f85a6db93e4c5ce6398158
Jacques <jacques.verre@stakion.io>
2021-01-04 08:46:15 +0000

    Remove files that don't need to be commited

--- build/lib/tensorboard_projects/types/__init__.py
+++
@@ -1,3 +0,0 @@
-from .initialization import create_model_metadata
-
-__all__ = ["create_model_metadata"]
--- build/lib/tensorboard_projects/types/initialization.py
+++
@@ -1,9 +0,0 @@
-def create_model_metadata(model_metadata):
-    return {
-        'model_id': model_metadata.get('model_id', ''),
-        'model_name': model_metadata.get('model_name', ''),
-        'description': model_metadata.get('description', ''),
-        'path': model_metadata.get('path', ''),
-        'archived_runs': model_metadata.get('archived_runs', []),
-        'deleted_runs': model_metadata.get('deleted_runs', []),
-    }
